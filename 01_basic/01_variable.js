8a94cf72048ef36289ceff6208e6632470b18513
Bittu kumar prajapati <109884531+Bittu-kumar2003@users.noreply.github.com>
2024-03-25 15:04:59 +0530

    Add files via upload

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = "12345";
+let accountEmail = "bittu@gmail.com";
+var accountpassword = "Kumar@.123";
+accountCity = "jaipur";
+let accountState;
+
+// Try to change the value 
+
+accountEmail="bittu2003@2gmail.com";
+accountpassword="45678";
+accountCity="Hajipur";
+// accountId="45672"     ( because it's use the const keyword means not change the value )
+console.table([accountEmail,accountpassword,accountCity,accountState]);
