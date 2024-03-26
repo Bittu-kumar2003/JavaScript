ec7e08301b5d34ac2e0dab31774673bfd836df5b
Bittu kumar prajapati <109884531+Bittu-kumar2003@users.noreply.github.com>
2024-03-26 16:02:31 +0530

    Update dataType_summeries.js

--- 01_basic/dataType_summeries.js
+++ 01_basic/dataType_summeries.js
@@ -33,3 +33,28 @@ const myFunction = function () {
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+// stack (primitive),  Heap(Npon-primitive)
+
+let myName="Bittu prajapti"
+let anotherName = myName;
+anotherName ="Dishikant prajapati"
+console.log(myName)
+console.log(anotherName);
+
+
+let userOne={
+  email:"bittu@gmail.com",
+  upi:"bittu@ybl"
+}
+
+let userTwo=userOne
+userTwo.email="bittuprajapati2003@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+// Notes 
+// stack give the copy means bluePrint but Heap is given the reference 
