a32f1942c6d16849e7cd5c2be6ea038ed9ad3129
Bittu kumar prajapati <109884531+Bittu-kumar2003@users.noreply.github.com>
2024-03-26 16:01:43 +0530

    Add files via upload

---
+++ 01_basic/05_String.js
@@ -0,0 +1,34 @@
+const name = "Bittu";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("bittu-dc-com");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("t"));
+
+const newString = gameName.substring(0, 4);          // substring use the (particition)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);          // slice use the (reverse)
+console.log(anotherString);
+
+const newStringOne = "   Bittu    "; 
+console.log(newStringOne);
+console.log(newStringOne.trim());                    // trim use the negelated the space 
+
+const url = "https://hitesh.com/hitesh%20choudhary";
+
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("sundar"));
+
+console.log(gameName.split("-"));                        // divide the task its depends the command
