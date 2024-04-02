ca5d730f0cc5a41344b6bfea2a3980d7e909c918
Bittu kumar prajapati <109884531+Bittu-kumar2003@users.noreply.github.com>
2024-04-02 17:38:55 +0530

    Add files via upload

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {                  // switch syntx
+//     case value: 
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const months = 3;
+
+switch (months) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+}
+
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("feb");
+    break;
+  case "march":
+    console.log("march");
+    break;
+  case "april":
+    console.log("april");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+}
