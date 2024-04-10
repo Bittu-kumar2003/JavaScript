bf565663f6f3b549e03eb456d3b98a0d6605f31d
Bittu kumar prajapati <109884531+Bittu-kumar2003@users.noreply.github.com>
2024-04-10 23:24:38 +0530

    Add files via upload

---
+++ Project/Color_change/app.js
@@ -0,0 +1,47 @@
+// const buttons = document.querySelectorAll(".buttton");
+// const body = document.querySelector("body");
+// buttons.forEach(function (button) {
+//   button.addEventListener('click', function (e) {
+//     console.log(e);
+//     console.log(e.target);
+//     if (e.target.id === "grey") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+
+// try purpurse
+// const buttons = document.querySelectorAll(".button");
+// const body = document.querySelector("body");
+
+// buttons.forEach(function (button) {
+//   console.log(button);
+//   button.addEventListener("click", function (e) {
+//     console.log(e);
+//     console.log(e.target);
+//     if (e.target.id === "grey") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//   });
+// });
+
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
